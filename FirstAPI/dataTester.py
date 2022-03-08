059b506f8590998a74a79ecedaffee976a63b170
Abhi6 <31413154+Abhi6@users.noreply.github.com>
2022-03-07 23:57:17 -0500

    Delete dataTester.py

--- FirstAPI/dataTester.py
+++
@@ -1,5 +0,0 @@
-import json
-with open('/Users/abhinavmara/Desktop/Python/FirstAPI/COVID19_stats.json','r') as f:
-    data_dict = json.load(f)
-
-print(data_dict['States']['Virginia']['Counties']['Henrico County'])
